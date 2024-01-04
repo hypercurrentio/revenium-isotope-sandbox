51accc0d956d5bc72a83f02dead4d30ea0bb4acd
John D'Emic <john.demic@hypercurrent.io>
2024-01-03 21:47:37 -0500

    Misc fixes: - Removed unncessary bridge network definition from docker-compose.yml - Removed trailing slash causing redirects from Flask app - Fixed port for isotope binary

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/echo/', methods=['POST'])
+@app.route('/echo', methods=['POST'])
 def echo():
     # Echo back the received data
     data = request.json
